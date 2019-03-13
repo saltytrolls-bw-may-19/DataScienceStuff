65158d04003c7b78bba7ef8bcdec4064167eec24
ed.chin.git <edgardochin@gmail.com>
2019-03-13 17:13:22 -0500

    added CORS

--- TWpred/app.py
+++ TWpred/app.py
@@ -1,9 +1,11 @@
 """ Main application and routing logic for TweetR """
 from flask import Flask, request, render_template
 from functions import textblob_sentiment
+from flask_cors import CORS
 
 """ create + config Flask app obj """
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def root():
