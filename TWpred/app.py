0b6a87cba3ae145b39e3924ae6687cf123a6d282
ed.chin.git <edgardochin@gmail.com>
2019-03-13 14:42:14 -0500

    app.py entry point

--- TWpred/app.py
+++ TWpred/app.py
@@ -2,20 +2,21 @@
 from flask import Flask, request, render_template
 from .functions import textblob_sentiment
 
-def create_app():
-    """ create + config Flask app obj """
-    app = Flask(__name__)
+""" create + config Flask app obj """
+app = Flask(__name__)
 
-    @app.route('/')
-    def root():
-        return render_template('base.html', title='Home')
+@app.route('/')
+def root():
+    return render_template('base.html', title='Home')
 
-    @app.route('/user/<name>')
-    def user(name=None):
-        json_out = textblob_sentiment(name)
-        return json_out
-    return app
-       
+@app.route('/user/<name>')
+def user(name=None):
+    json_out = textblob_sentiment(name)
+    return json_out
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+
 #  to run from terminal : cd to TWpred subfolder (where app.py resides)
 #                         set FLASK_APP=TWpred:APP
 #                   +     flask run   OR    flask shell
