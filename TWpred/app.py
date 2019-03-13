c688461554e27729a52957a8bd217b63fa5fcd27
ed.chin.git <edgardochin@gmail.com>
2019-03-13 10:21:14 -0500

    Mar 13 10:21

--- TWpred/app.py
+++ TWpred/app.py
@@ -1,44 +1,21 @@
 """ Main application and routing logic for TweetR """
 from flask import Flask, request, render_template
-from .models import DB, User, Tweet
-from decouple import config
 from .functions import textblob_sentiment
-from .predicted import predict_user
-import re
-import sys
-import pandas as pd
-from textblob import TextBlob
-from google.cloud import bigquery
 
 def create_app():
     """ create + config Flask app obj """
     app = Flask(__name__)
 
-    #  after creatin models.py  run the follow
-    #  configure the app object 
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')  # get db loc from .env
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    DB.init_app(app)
-
-    client = bigquery.Client
     @app.route('/')
     def root():
         return render_template('base.html', title='Home')
 
-    #@app.route('/user', methods=['POST'])
     @app.route('/user/<name>')
     def user(name=None):
-        message = ''
         json_out = textblob_sentiment(name)
         return json_out
-        # return render_template('user.html', title=name, message=message)
-
     return app
-
        
 #  to run from terminal : cd to TWpred subfolder (where app.py resides)
 #                         set FLASK_APP=TWpred:APP
 #                   +     flask run   OR    flask shell
-
- 
--- TWpred/functions.py
+++ TWpred/functions.py
@@ -6,60 +6,7 @@ import sys
 import pandas as pd
 from textblob import TextBlob
 from google.cloud import bigquery
-from TWpred.twitter_API import *
 
-
-def adduser(tw_handle):
-   #   Get TW user info 
-   twitter_user = TWITTER.get_user(tw_handle)
-
-   #  get their tweets from TW timeline
-   tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, tweet_mode='extended')
-
-   # create a DB User -  use the model.py class
-   db_user = User(id=twitter_user.id, name=twitter_user.screen_name, newest_tweet_id=tweets[0].id)
-
-   # load the tweets
-   for tweet in tweets:
-      embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-      db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
-      DB.session.add(db_tweet)        #  add the tweets to the DB first
-      db_user.tweets.append(db_tweet) # then connect/append to the user
-   
-   DB.session.add(db_user)  # can this be done earlier??
-   DB.session.commit()
-   #------
-   return
-
-
-def add_or_update_user(username):
-
-   """Add or update a user *and* their Tweets, error if no/private user."""
-   try:
-      twitter_user = TWITTER.get_user(username)
-      #  if userId exists .get it  or if not create one 
-      db_user = (User.query.get(twitter_user.id) or
-                  User(id=twitter_user.id, name=username))
-      DB.session.add(db_user)
-      # We want as many recent non-retweet/reply statuses as we can get
-      tweets = twitter_user.timeline(
-         count=200, exclude_replies=True, include_rts=False,
-         tweet_mode='extended', since_id=db_user.newest_tweet_id)
-      if tweets:
-         db_user.newest_tweet_id = tweets[0].id
-      for tweet in tweets:
-         # Get embedding for tweet, and store in db
-         embedding = BASILICA.embed_sentence(tweet.full_text,
-                                             model='twitter')
-         db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
-                           embedding=embedding)
-         db_user.tweets.append(db_tweet)
-         DB.session.add(db_tweet)
-   except Exception as e:
-      print('Error processing {}: {}'.format(username, e))
-      raise e
-   else:
-      DB.session.commit()
 
 def clean_text(text):
     """
