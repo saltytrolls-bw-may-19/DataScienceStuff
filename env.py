f84a0a542fc4a4bd7a1304904f4c598b7debe090
Kevin Brack <brackkevin@gmail.com>
2019-03-16 21:58:41 -0500

    Remove creds

--- env.py
+++ env.py
@@ -1,12 +1,1 @@
-{
-  "type": "service_account",
-  "project_id": "reflecting-poet-233202",
-  "private_key_id": "b5c7be8cd67d460f8c6c59033f2ee81059cf4406",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtfRLqq23PP+l1\nHEEMR1BjK7m2aanXr3+gxeZLOoObcgQQouK1/ZsVSRQ2xmyqlfRZAlmJqAtik48/\nJVrixSPTWCeNvvbANP+Si6+hdqtxzV0xnWR/Uj4makahf/CSx8Tq7p2N5RYOh05c\nHWiShEvIs/vDcgCEsRf1eUnv1CHBaOZB0ZKh/rmtOO+yGBAlLwJra5eccdqja2JZ\n4i7zm7oAvo0FXl8JNOa7LRNUz9CUm+R4vI1NsxNYFIEf17jnHnOkm1W4kfWqVqAw\n8qaNXwDIiwd8qZmMCXMNkcWwzkwHwYgRmT6gv1qWd5yFf/BkwSqdl/Se0oMLmBJF\npfEu6ocBAgMBAAECggEAEynj90qY7mH9sIs+rzPIcC/yC3GICpRu+Wwq2Gf+BiN1\nyf2oI9i/8v23zyYjdkNqspNVHyTSHyE0G1JNxAtpPez4r4pm2Mvx6DboX2JLpV49\nO2AKrBrCVT17kFbg1OHewWG6wyQJR/EPyZKRDNAyy9h/yLLU1lN2lU3Ayk5pLSMx\n7HdZxEGRkQqDre/j+bog66Sur86CmMYhe1lxc65fdGMUtRt7taZIpfhgFtnRIIPL\nH+YpfQzFF2beCkripVcbtjJCV4wm67iM8zxsNv6z7VhOg1n6d+RzDn4hbWE4xxg/\nAhDdj4uKs97HJch8SGGhdHGakbBFGvtgCJVtQ3OXsQKBgQDXOxNza4/VRdiDevoD\nrnGfAMFXdrSEDGD3vVMB3dUiRcDVzDWTUsRfOsEA93HMBMiykFA3nGr134yiS1nG\nbArBuZ2Hv8hHG13X712J2rxa+mf1YQPEtM4bWoqNrtnDP8et9d8TVMkOVyImG/Kx\naHYNgeWdKKLF7EMokXkBrPcEyQKBgQDOWdgmSApzU9O3G0aKyw8EooIRGP5CRdt9\nZhOkrXY1UwXzr92PH2fGe4zTDGqUy3wb8xw7sYCerisvx1SwmcxXBBdYD70MtSEW\n+zTJHutJG91tw0JBlftGnwPctZxMcOl3b2QlCmnfUMJw8BEqIE3735YxQnBRh/QC\nbsTFZRUkeQKBgQC/3GyxZf/4uWORiS2X3IhymiS/yBSQ5cwG6+Ez98DH5XBJ5dkY\ndB6TWXe1Wd86ZEVYUCcB+9CpT1c5UXw80hEgsksIOODa6j1UgY9blP51B2Vmqs3G\nKvrUDXkXZfe/AnsIyvXJcLotq2TInf9RQHKEBfheiVvYnY0ce9lnsf0daQKBgEmQ\ndiKPpEohJGUu8CMi6SO526zEwEBYLzuxXGdk/7Na+dA6bj5L7+8utLG4x3ZsZL6C\nGRqe2T+4z+Skp2ieK98JsljOo5MYL8fFnrfksfTKXlJ3PfOryH0Qy4uvox0NU+4k\nZAtgfwYCAI/KxhPCmIqm1yCQkwI9+Zu0aaoFs1fhAoGAfQEn1KinwKjgrJOJPVom\neS3vWJR2ukgNwjaWc+09r2BcjIPXtzYze+fKhJUVEvEqE+HBTape3Rtpd3BtbiwS\nhigDaNDAo9OB9+rmkNOFPDSj0SzHdRTb6CrDuYparuC2VAGqUoApjh8Lg8e7qt3w\noELTYS9bWKlTrisvTIYcFmA=\n-----END PRIVATE KEY-----\n",
-  "client_email": "saltytrolls@reflecting-poet-233202.iam.gserviceaccount.com",
-  "client_id": "104377059500084189806",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/saltytrolls%40reflecting-poet-233202.iam.gserviceaccount.com"
-}
+replace this text with actual google credentials
